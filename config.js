22e8e8a38ab8256850e88ff03e5ad69a2fff0ebf
wanglei288 <wanglei59@jd.com>
2017-08-17 13:38:45 +0800

    init

--- config.js
+++ config.js
@@ -1,23 +1,43 @@
 module.exports = {
-	title: 'Zing Gallery',
-	wording: {
-		noAccess: '抱歉，你没有权限访问'
-	},
-	albums: {
-		"贵阳": {
-		  thumbnail : "IMG_0331.JPG",
-		  sort: 1
-		}, 
-		"千户苗寨": {
-		  description : "没有什么能够阻挡",
-		  thumbnail : "IMG_0424.jpg",
-		  name: "千户苗寨"
-		},
-		"私密": {
-		  description : "私密",
-		  name: "私密",
-		  password: "233",
-			passwordTips: "密码是233"
-		}
-	}
+  title: 'Zing Gallery',
+  wording: {
+    noAccess: '抱歉，你没有权限访问'
+  },
+  albums: {
+    "贵阳": {
+      thumbnail: "IMG_0331.JPG",
+      sort: 1
+    },
+    "千户苗寨": {
+      description: "没有什么能够阻挡",
+      thumbnail: "IMG_0424.jpg",
+      name: "千户苗寨"
+    },
+    "私密": {
+      description: "私密",
+      name: "私密",
+      password: "233",
+      passwordTips: "密码是233"
+    },
+    "动物": {
+      description: "没有什么能够阻挡",
+      thumbnail: "Koala.jpg",
+      name: "动物"
+    },
+    "花草树木": {
+      description: "没有什么能够阻挡",
+      thumbnail: "Tulips.jpg",
+      name: "花草树木"
+    },
+    "草原": {
+      description: "没有什么能够阻挡",
+      thumbnail: "IMG_0424.jpg",
+      name: "草原"
+    },
+    "天空": {
+      description: "没有什么能够阻挡",
+      thumbnail: "Desert.jpg",
+      name: "天空"
+    }
+  }
 }
